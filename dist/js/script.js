f70290a81ca2d827e0f3895cfa94e5349a7785ba
Rostyslav <RostikL1994@gmail.com>
2023-11-24 23:56:03 +0300

    reviews section - done

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 2,
+        navigation: {
+            nextEl: '.reviews__arrow--next',
+            prevEl: '.reviews__arrow--prev',
+        },
+
+    });
+
+});
