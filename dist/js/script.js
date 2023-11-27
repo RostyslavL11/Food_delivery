0044fb050632288f3ec7ea10d1cff2b0bdeee1e4
Rostyslav <RostikL1994@gmail.com>
2023-11-27 14:59:39 +0300

    working witn nav many
    problem with button in ul list

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const swiper = new Swiper('.swiper', {
-        slidesPerView: 2,
-        navigation: {
-            nextEl: '.reviews__arrow--next',
-            prevEl: '.reviews__arrow--prev',
-        },
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 2,
+		navigation: {
+			nextEl: '.reviews__arrow--next',
+			prevEl: '.reviews__arrow--prev',
+		},
 
-    });
+	});
+
+	// бургер 
+	const burger = document.querySelector('.burger');
+	const menu = document.querySelector('.menu');
+
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('burger_active')
+		menu.classList.toggle('open');
+	})
+
 
 });
